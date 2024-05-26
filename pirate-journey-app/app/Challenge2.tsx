3ef2f902da40141e1b5d50db74145b6ee5a4dede
Nizmo_o <etu43306@gmail.com>
2024-05-26 02:35:18 +0200

    feat: Add graphics components & challenge success handle for Challenge 2

--- pirate-journey-app/app/Challenge2.tsx
+++ pirate-journey-app/app/Challenge2.tsx
@@ -1,37 +1,109 @@
 import React, {useState, useEffect} from "react";
-import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { Accelerometer } from 'expo-sensors';
-import { NativeScreen } from "react-native-screens";
-import * as Progress from 'react-native-progress';
+import { Button, StyleSheet, Text, View, TouchableOpacity, ImageBackground } from "react-native";
+import { useSelector } from 'react-redux';
+import { useDispatch } from "react-redux";
+
+// Components
 import CannonProgressBar from "../components/CannonProgressBar";
+import ChallengeIntro from "@/components/ChallengeIntro";
+import CustomModal from "@/components/CustomModal";
+import CustomButton from "@/components/CustomButton";
+import HelpButton from "@/components/HelpButton";
+
+// Redux Store
+import { unlock } from "@/store/lastUnlockedChallenge";
 
 export default function Challenge2({navigation}) {
   const [isChallengeStarted, setIsChallengeStarted] = useState(false)
+  const [challengeDone, setChallengeDone] = useState(false);
+  const [modalHelpVisible, setModalHelpVisible] = useState(false);
+  const [modalSuccessVisible, setModalSuccessVisible] = useState(false);
+  const dispatch = useDispatch(); // Redux
 
-  // useEffect(() => {
-  //   _subscribe();
-  //   setPrevData({prevX: x, prevY: y, prevZ: z});
-  //   return () => _unsubscribe();
-  // }, [prevX, x]);
+  const textIntro = "Voici l'introduction du Challenge 2";
+  const helpText = "Voici l'indice du Challenge 2, bonne chance !";
+  const helpTitle = "Indice";
+  const challengePassedModalTitle = 'Épreuve 2 réussie !'
+  const challengePassedModalText = 'Bravo, tu as réussi l\'épreuve 2. Tu peux maintenant passer à l\'épreuve suivante';
 
+
+  const handleModalClose = () => {
+    setModalSuccessVisible(false);
+    if (challengeDone) {
+      navigation.navigate('Home');
+    }
+  }
+
+  const handleHelpPress = () => {
+    setModalHelpVisible(true);
+  };
+
+const handleChallengeDone = () => {
+  setChallengeDone(true);
+  setModalSuccessVisible(true);
+  dispatch(unlock(3))
+};
+  
   return (
     <View style={styles.container}>
-      <Button
-        title = "start challenge"
-        onPress={() => (setIsChallengeStarted(true))}
-      />
-      {isChallengeStarted ? <CannonProgressBar/> : null}
+      {isChallengeStarted ? 
+          <ImageBackground source={require("@/assets/images/challenge2-carousel.png")} style={styles.background}>
+            <CannonProgressBar onChallengeDone={handleChallengeDone}/>
+            <HelpButton style={styles.helpButton} onPressImage={handleHelpPress}/>
+            <CustomModal 
+              modalVisible={modalHelpVisible} 
+              setModalVisible={setModalHelpVisible} 
+              title={helpTitle} 
+              text={helpText} 
+              position={"flex-end"}
+            />
+            <CustomModal 
+              modalVisible={modalSuccessVisible} 
+              setModalVisible={setModalSuccessVisible} 
+              title={challengePassedModalTitle} 
+              text={challengePassedModalText} 
+              onClose={handleModalClose}
+              position="flex-end"
+            />
+          </ImageBackground> 
+          : 
+          <View style={styles.container}>
+            <ChallengeIntro
+              backgroundImage={require("@/assets/images/challenge2-intro.png")}
+              introText={textIntro}
+              helpText={helpText}
+            />
+            <View style={styles.buttonContainer}>
+              <CustomButton title="Commencer" onPress={() => setIsChallengeStarted(true)} />
+            </View>
+          </View>
+          }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    element: {
-        margin: 10,
-    },
+  container: {
+    flex: 1,
+  },
+  element: {
+      margin: 10,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  background: {
+    flex: 1,
+  },
+  helpButton: {
+    position: 'absolute',
+    margin: "5%",
+    top: "5%",
+    right: 0,
+    alignSelf: 'flex-end',
+  }
 });
